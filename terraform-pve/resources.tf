8850dba7eb78a55a2d8a570e1dff811603e117c5
mikolajtkaczyk64 <112934589+mikolajtkaczyk64@users.noreply.github.com>
2024-12-20 12:53:55 +0100

    Update resources.tf

--- terraform-pve/resources.tf
+++ terraform-pve/resources.tf
@@ -5,3 +5,97 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_22_04_server_cloudi
   file_name    = "ubuntu-22.04-server-cloudimg-amd64.img"
   url          = "https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img"
 }
+
+resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
+  name        = "terraform-provider-proxmox-ubuntu-vm"
+  description = "Managed by Terraform"
+  tags        = ["terraform", "ubuntu"]
+
+  node_name = "first-node"
+  vm_id     = 4321
+
+  agent {
+    # read 'Qemu guest agent' section, change to true only when ready
+    enabled = false
+  }
+  # if agent is not enabled, the VM may not be able to shutdown properly, and may need to be forced off
+  stop_on_destroy = true
+
+  startup {
+    order      = "3"
+    up_delay   = "60"
+    down_delay = "60"
+  }
+
+  cpu {
+    cores        = 2
+    type         = "x86-64-v2-AES"  # recommended for modern CPUs
+  }
+
+  memory {
+    dedicated = 2048
+    floating  = 2048 # set equal to dedicated to enable ballooning
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_22_04_server_cloudimg_amd64_img.id
+    interface    = "scsi0"
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "10.10.198.105/24"
+        gateway = "10.10.198.1"
+      }
+    }
+
+    user_account {
+      keys     = [trimspace(tls_private_key.ubuntu_vm_key.public_key_openssh)]
+      password = random_password.ubuntu_vm_password.result
+      username = "ubuntu"
+    }
+
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  tpm_state {
+    version = "v2.0"
+  }
+
+  serial_device {}
+}
+
+resource "random_password" "ubuntu_vm_password" {
+  length           = 16
+  override_special = "_%@"
+  special          = true
+}
+
+resource "tls_private_key" "ubuntu_vm_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+output "ubuntu_vm_password" {
+  value     = random_password.ubuntu_vm_password.result
+  sensitive = true
+}
+
+output "ubuntu_vm_private_key" {
+  value     = tls_private_key.ubuntu_vm_key.private_key_pem
+  sensitive = true
+}
+
+output "ubuntu_vm_public_key" {
+  value = tls_private_key.ubuntu_vm_key.public_key_openssh
+}
