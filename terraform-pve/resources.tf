a952b22a5fe700d054431f41a309f2da11152d2b
mikolajtkaczyk64 <112934589+mikolajtkaczyk64@users.noreply.github.com>
2024-12-20 15:25:41 +0100

    Update resources.tf

--- terraform-pve/resources.tf
+++ terraform-pve/resources.tf
@@ -84,12 +84,3 @@ output "ubuntu_vm_password" {
   value     = nonsensitive(random_password.ubuntu_vm_password.result)
   sensitive = true
 }
-
-output "ubuntu_vm_private_key" {
-  value     = tls_private_key.ubuntu_vm_key.private_key_pem
-  sensitive = true
-}
-
-output "ubuntu_vm_public_key" {
-  value = tls_private_key.ubuntu_vm_key.public_key_openssh
-}
